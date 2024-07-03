31281751f0737316509fbc6e7dcdaaf28fdc21ef
mgkcloud <will@feistyagency.com>
2024-07-03 17:17:48 +1000

    disable anoying ios tings

--- app/page.js
+++ app/page.js
@@ -7,15 +7,27 @@ import DealerSection from '@/components/game/DealerSection';
 // import { BubbleChat } from 'flowise-embed-react';
 
 export default function Cards() {
+  // Disable context menu on long press
+  React.useEffect(() => {
+    const disableContextMenu = (event) => {
+      event.preventDefault();
+    };
+
+    document.addEventListener('contextmenu', disableContextMenu);
+
+    return () => {
+      document.removeEventListener('contextmenu', disableContextMenu);
+    };
+  }, []);
 
   return (
-    <main className='w-[100vw] overflow-hidden bg-neutral text-neutral-content'>
+    <main className='w-[100vw] overflow-hidden bg-neutral text-neutral-content' style={{ userSelect: 'none', WebkitUserSelect: 'none' }}>
       <Player>
         {({ user, visibleCards, deckCards, moveCardToDeck, moveCardToHand, isLoading, onAddNewCards, onClearCards, setVisibleCards, setDeckCards, onClearHand }) => (
           <>
             {/* <BubbleChat chatflowid="8ee9b276-744b-4838-b1b7-9f0561d0b65b" apiHost="http://supa.centaur-cloud.ts.net:3000" /> */}
             <DealerSection
-            className='max-h-[100%]'
+              className='max-h-[100%]'
               handCards={visibleCards}
               deckCards={deckCards}
               onMoveCardToDeck={moveCardToDeck}
