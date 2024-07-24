10d8927fda26e7933461744318213382c8bc8b9d
mgkcloud <will@feistyagency.com>
2024-07-24 10:07:18 +1000

    yt

---
+++ app/testing/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import config from "@/config";
+import ButtonLead from "@/components/ButtonLead";
+
+
+
+const Test = () => {
+  return (
+    <main className="w-[100vw] h-[100vh] mx-auto bg-black flex justify-center align-center pt-[400px]">
+
+<ButtonLead className="m-auto " />
+
+    </main>
+  );
+};
+
+export default Test;
